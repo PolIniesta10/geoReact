60c5a6d781369628cfa8556e72e122f4f2455301
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2023-01-23 17:09:16 +0100

    Login/Register

--- geoReact/src/auth/Login.jsx
+++ geoReact/src/auth/Login.jsx
@@ -1,38 +1,67 @@
 import React from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import { faGooglePlusG } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
 export default function Login({ setLogin }) {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  const sendLogin = (e) => {
+    e.preventDefault();
+
+    console.log("Comprovant credencials....");
+    // Enviam dades a l'aPI i recollim resultat
+    fetch("https://backend.insjoaquimmir.cat/api/login", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({ email: email, password: password })
+    })
+      .then((data) => data.json())
+      .then((resposta) => {
+        console.log(resposta);
+        if (resposta.success === true) {
+          alert(resposta.authToken);
+        }
+      })
+      .catch((data) => {
+        console.log(data);
+        alert("Catchch");
+      });
+
+    alert("He enviat les Dades:  " + email + "/" + password);
+  };
     return (
       <>
-        <div class="container" id="container">
-          <div class="form-container sign-in-container">
+        <div className="container" id="container">
+          <div className="form-container sign-in-container">
             <form action="#">
-              <h1>Sign in</h1>
-              <div class="social-container">
-              <a href="#" class="social"><i><FontAwesomeIcon icon={faFacebookF}/></i></a>
-                <a href="#" class="social"><i><FontAwesomeIcon icon={faGooglePlusG}/></i></a>
-                <a href="#" class="social"><i><FontAwesomeIcon icon={faTwitter}/></i></a>
+              <h1>Iniciar sesion</h1>
+              <div className="social-container">
+                <a href="#" className="social"><i><FontAwesomeIcon icon={faFacebookF}/></i></a>
+                <a href="#" className="social"><i><FontAwesomeIcon icon={faGooglePlusG}/></i></a>
+                <a href="#" className="social"><i><FontAwesomeIcon icon={faTwitter}/></i></a>
               </div>
-              <span>or use your account</span>
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <a href="#">Forgot your password?</a>
-              <button class="SignBtn">Sign In</button>
+              <span>o usa tu cuenta ya existente</span>
+              <input name="email" type="email" placeholder="Email" onChange={(e) => {setEmail(e.target.value);}} />
+              <input name="password" type="password" placeholder="Contraseña" onChange={(e) => {setPassword(e.target.value);}}/>
+              <a href="#">Olvidaste tu contraseña?</a>
+              <button className="SignBtn" onClick={(e) => {sendLogin(e.target.value);}}>Iniciar sesion</button>
             </form>
           </div>
-          <div class="overlay-container">
-            <div class="overlay">
-              <div class="overlay-panel overlay-right">
-                <h1>Hello, Friend!</h1>
-                <p>Enter your personal details and start journey with us</p>
-                <button className="SignBtn ghost" id="signIn" onClick={() => {
-                  setLogin(false);
-                  }}
-                >
-                  Registrate
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-right">
+                <h1>Hola, amigo!</h1>
+                <p>Ingresa tus datos personales y comienza tu viaje con nosotros</p>
+                <button className="SignBtn ghost" id="signIn" onClick={() => {setLogin(false);}}>Registrate
                 </button>
+                alert(message.alert)
               </div>
             </div>
           </div>
--- geoReact/src/auth/Register.jsx
+++ geoReact/src/auth/Register.jsx
@@ -1,38 +1,91 @@
 import React from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import { faGooglePlusG } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
 export default function Register({ setLogin }) {
+  let [formulari, setFormulari] = useState({});
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setFormulari({
+      ...formulari,
+      [e.target.name]: e.target.value
+    });
+  };
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    let { name, password, password2, email } = formulari;
+    console.log(
+      "He enviat les Dades:  " +
+        name +
+        "/" +
+        email +
+        "/" +
+        password +
+        "/" +
+        password2
+    );
+      
+    if (password2 !== password) {
+      alert("La contraseña no coincide");
+      return false;
+    }
+
+    fetch("https://backend.insjoaquimmir.cat/api/register", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      // Si els noms i les variables coincideix, podem simplificar
+      body: JSON.stringify({ name, email, password })
+    })
+      .then((data) => data.json())
+      .then((resposta) => {
+        console.log(resposta);
+        if (resposta.success === true) {
+          alert(resposta.authToken);
+        }
+      })
+      .catch((data) => {
+        console.log(data);
+        alert("Catchch");
+      });
+
+      alert("He enviat les Dades:  " + email + "/" + password);
+
+  };
+
     return (
       <>
-        <div class="container right-panel-active" id="container">
-          <div class="form-container sign-up-container">
+        <div className="container right-panel-active" id="container">
+          <div className="form-container sign-up-container">
             <form action="#">
-              <h1>Create Account</h1>
-              <div class="social-container">
-                <a href="#" class="social"><i><FontAwesomeIcon icon={faFacebookF}/></i></a>
-                <a href="#" class="social"><i><FontAwesomeIcon icon={faGooglePlusG}/></i></a>
-                <a href="#" class="social"><i><FontAwesomeIcon icon={faTwitter}/></i></a>
+              <h1>Crear cuenta</h1>
+              <div className="social-container">
+                <a href="#" className="social"><i><FontAwesomeIcon icon={faFacebookF}/></i></a>
+                <a href="#" className="social"><i><FontAwesomeIcon icon={faGooglePlusG}/></i></a>
+                <a href="#" className="social"><i><FontAwesomeIcon icon={faTwitter}/></i></a>
               </div>
-              <span>or use your email for registration</span>
-              <input type="text" placeholder="Name" />
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <button class="SignBtn" >Sign Up</button>
+              <span>o use su correo electrónico para registrarse</span>
+              <input name="name" type="text" placeholder="Nombre" onChange={handleChange}/>
+              <input name="email" type="email" placeholder="Email" onChange={handleChange}/>
+              <input name="password" type="password" placeholder="Contraseña" onChange={handleChange}/>
+              <input name="password2" type="password" placeholder="Repite contraseña" onChange={handleChange}/>
+              <button className="SignBtn margin-top" onClick={(e) => {handleRegister(e);}}>Registrarse</button>
             </form>
           </div>
-          <div class="overlay-container">
-            <div class="overlay">
-              <div class="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
-                <p>To keep connected with us please login with your personal info</p>
-                <button className="SignBtn ghost" id="signUp" onClick={() => {
-                  setLogin(true);
-                  }}
-                >
-                  Inicia sesion
-                </button>
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
+                <h1>Bienvenido de nuevo!</h1>
+                <p>Para mantenerse conectado con nosotros, inicie sesión con su información personal</p>
+                <button className="SignBtn ghost" id="signUp" onClick={() => {setLogin(true);}}>Iniciar sesion</button>
               </div>
             </div>
           </div>
