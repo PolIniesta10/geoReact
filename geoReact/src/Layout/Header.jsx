b39850bc2a6aeb77bafd9ad145b07b3c4335abe1
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2023-01-24 20:22:44 +0100

    css header

--- geoReact/src/Layout/Header.jsx
+++ geoReact/src/Layout/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FiLogOut } from 'react-icons/fi';
+import { TiInfoLargeOutline } from 'react-icons/ti';
+import { MdOutlinePlace } from 'react-icons/md';
+import { AiOutlinePicture } from 'react-icons/ai';
 
 
 export default function Header() {
@@ -7,12 +11,13 @@ export default function Header() {
   return (
     <div className='cajamasterheader'>
       <div className='izq_header'>
-          <div className='linksheader'><Link to="/places">Places</Link></div>
-          <div className='linksheader'><Link to="/posts">Posts</Link></div>
-          <div className='linksheader'><Link to="/about">About</Link></div>
+          <div className='linksheader'><Link to="/places"><a><MdOutlinePlace /></a><a>Places</a></Link></div>
+          <div className='linksheader'><Link to="/posts"><a><AiOutlinePicture /></a><a>Posts</a></Link></div>
+          <div className='linksheader'><Link to="/about"><a><TiInfoLargeOutline /></a><a>About</a></Link></div>
       </div>
       <div className='der_header'>
-
+          <div className='user__header'>Administrador ( author editor admin )</div>
+          <button className='logout__header'><FiLogOut /></button>
       </div>
     </div>
   )
