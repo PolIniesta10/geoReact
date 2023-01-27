aed8552ef88cd9088ec6b7316a9df393d4a42495
Pol Iniesta González <poingo@fp.insjoaquimmir.cat>
2023-01-27 17:44:57 +0100

    css header

--- geoReact/src/Layout/Header.jsx
+++ geoReact/src/Layout/Header.jsx
@@ -11,9 +11,9 @@ export default function Header() {
   return (
     <div className='cajamasterheader'>
       <div className='izq_header'>
-          <div className='linksheader'><Link to="/places"><a><MdOutlinePlace /></a><a>Places</a></Link></div>
-          <div className='linksheader'><Link to="/posts"><a><AiOutlinePicture /></a><a>Posts</a></Link></div>
-          <div className='linksheader'><Link to="/about"><a><TiInfoLargeOutline /></a><a>About</a></Link></div>
+          <Link to="/places"><div className='linksheader'><a><MdOutlinePlace /></a><a>Places</a></div></Link>
+          <Link to="/posts"><div className='linksheader'><a><AiOutlinePicture /></a><a>Posts</a></div></Link>
+          <Link to="/about"><div className='linksheader'><a><TiInfoLargeOutline /></a><a>About</a></div></Link>
       </div>
       <div className='der_header'>
           <div className='user__header'>Administrador ( author editor admin )</div>
