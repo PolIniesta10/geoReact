88403257d6b0eb6617a1b28ce36ecb3e543c509c
osmepa <osmepa@fp.insjoaquimmir.cat>
2023-03-02 15:27:25 +0100

    punto 1.1

--- geoReact/src/hooks/useForm.js
+++ geoReact/src/hooks/useForm.js
@@ -1,36 +1,32 @@
-import { useState} from "react";
+import { useState } from 'react';
 
 export const useForm = (initialForm={}) => {
 
     let [formState, setFormState] = useState(initialForm);
     
     const onInputChange = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
+
+    
+    const { name, value } = target;
+    
+    setFormState({
+    
+    ...formState,
+  
+    
+    [name]: value,
+    
+    });
+    
+   
+    
+    };
+    
+    const onResetForm = ({})=> {
+        setFormState(initialForm)
+    }
+
+    return { ...formState, formState,onInputChange, onResetForm };
+    
     };
 
-        
-    const OnChangeForm = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
-
-    const OnResetForm = () => {
-        setFormState('');
-    };
-    
-    // ………………………
-    
-    // Podem afegir més mètodes
-    
-    // I s’hauran de retornar a continuació
-    
-    return { ...formState, formState, onInputChange, OnChangeForm, OnResetForm };
-    
-};
