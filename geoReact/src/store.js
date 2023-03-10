0e047b80a6e8ee7bc9b30c087f6b9bd67a503bcc
OSCAR MEJIAS <OSCAR@PC45.pansgranier.local>
2023-03-10 10:20:07 +0100

    3.1 terminado

--- geoReact/src/store.js
+++ geoReact/src/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import todosReducer from "./slices/todoSlice"
+
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    todos: todosReducer
+  },
 })
--- geoReact/src/todos/ToDo.jsx
+++ geoReact/src/todos/ToDo.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { deltodo, toggletodo } from "../slices/todoSlice";
 
-export const ToDo = ({key,todo,handleDelete,handleToggleTodo}) => {
-    console.log({todo})
+export const ToDo = ({todo/*,handleDelete,handleToggleTodo*/}) => {
+  const dispatch = useDispatch();
+
   return (
       <>
       <div className="todo">
-        <div className='todoText'>{todo.text}</div>
+        <div className='todoText'>{todo.description}</div>
         <div className="botonesTodo">
-            {(todo.done) ? <button onClick={ (e) => {handleToggleTodo(todo.id)}} className='buttonDoneToDo'>ToDO </button>:
+            {(todo.done) ? /*<button onClick={ (e) => {handleToggleTodo(todo.id)}} className='buttonDoneToDo'>ToDO </button>:
             <button onClick={ (e) => {handleToggleTodo(todo.id)}} className='buttonDoneToDo'> Done</button>
             }
-            <button onClick={ (e) => {handleDelete(todo.id)}} className='buttonDoneToDo'> DELETE</button>
+            <button onClick={ (e) => {handleDelete(todo.id)}} className='buttonDoneToDo'> DELETE</button>*/
+            <button onClick={ (e) => dispatch(toggletodo(todo.id))} className='buttonDoneToDo'>Not Done</button> :
+            <button onClick={ (e) => dispatch(toggletodo(todo.id))} className='buttonDoneToDo'>Done</button>}
+            <button onClick={() => dispatch(deltodo(todo.id))} className="buttonDoneToDo">Remove</button>
         </div>
         
       </div>
